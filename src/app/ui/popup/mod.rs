09394e29d34cec313f2f32347f617456b33249c1
L3af <L3afMeAlon3@gmail.com>
2021-05-28 07:47:02 +0000

    feat: implement all popup positions

--- src/app/ui/popup/mod.rs
+++ src/app/ui/popup/mod.rs
@@ -1,8 +1,12 @@
 use crossterm::event::{KeyCode, KeyModifiers};
-use tui::layout::Rect;
+use tui::layout::{Constraint, Direction, Layout, Rect};
 
 use super::prelude::Menu;
-use crate::app::{context::Context, event::Event, helper::{centered_rect, CenterPosition, CrosstermFrame}};
+use crate::app::{
+    context::Context,
+    event::Event,
+    helper::{centered_rect, CenterPosition, CrosstermFrame},
+};
 
 pub mod confirmation;
 pub mod message;
@@ -47,20 +51,138 @@ impl Default for PopupArea {
 }
 
 pub struct Popup {
-    menu:       Box<dyn Menu + Send>,
-    area:       PopupArea,
+    menu: Box<dyn Menu + Send>,
+    area: PopupArea,
 }
 
 impl Popup {
     pub fn get_area(&self, frame_size: Rect) -> Rect {
         match self.area {
             PopupArea::Dynamic(func) => func(frame_size),
-            PopupArea::Absolute(widget, height, pos) => match pos {
-                // TODO: Other positions
-                _ => centered_rect(
-                    CenterPosition::AbsoluteInner(widget, height),
+            PopupArea::Absolute(width, height, pos) => {
+                let max_size = centered_rect(
+                    CenterPosition::AbsoluteOutter(2, 2),
                     frame_size,
-                ),
+                );
+
+                match pos {
+                    PopupPosition::TopLeft => {
+                        let top = Layout::default()
+                            .direction(Direction::Vertical)
+                            .constraints([
+                                Constraint::Length(height),
+                                Constraint::Min(0),
+                            ])
+                            .split(max_size)[0];
+
+                        Layout::default()
+                            .direction(Direction::Horizontal)
+                            .constraints([
+                                Constraint::Length(width),
+                                Constraint::Min(0),
+                            ])
+                            .split(top)[0]
+                    },
+                    PopupPosition::Top => {
+                        let top = Layout::default()
+                            .direction(Direction::Vertical)
+                            .constraints([
+                                Constraint::Length(height),
+                                Constraint::Min(0),
+                            ])
+                            .split(max_size)[0];
+
+                        centered_rect(CenterPosition::AbsoluteInner(width, height), top)
+                    },
+                    PopupPosition::TopRight => {
+                        let top = Layout::default()
+                            .direction(Direction::Vertical)
+                            .constraints([
+                                Constraint::Length(height),
+                                Constraint::Min(0),
+                            ])
+                            .split(max_size)[0];
+
+                        Layout::default()
+                            .direction(Direction::Horizontal)
+                            .constraints([
+                                Constraint::Min(0),
+                                Constraint::Length(width),
+                            ])
+                            .split(top)[1]
+                    },
+                    PopupPosition::Left => {
+                        let left = Layout::default()
+                            .direction(Direction::Horizontal)
+                            .constraints([
+                                Constraint::Length(width),
+                                Constraint::Min(0),
+                            ])
+                            .split(max_size)[0];
+
+                        centered_rect(CenterPosition::AbsoluteInner(width, height), left)
+                    },
+                    PopupPosition::Center => centered_rect(
+                        CenterPosition::AbsoluteInner(width, height),
+                        max_size,
+                    ),
+                    PopupPosition::Right => {
+                        let right = Layout::default()
+                            .direction(Direction::Horizontal)
+                            .constraints([
+                                Constraint::Min(0),
+                                Constraint::Length(width),
+                            ])
+                            .split(max_size)[1];
+
+                        centered_rect(CenterPosition::AbsoluteInner(width, height), right)
+                    },
+                    PopupPosition::BottomLeft => {
+                        let bottom = Layout::default()
+                            .direction(Direction::Vertical)
+                            .constraints([
+                                Constraint::Min(0),
+                                Constraint::Length(height),
+                            ])
+                            .split(max_size)[1];
+
+                        Layout::default()
+                            .direction(Direction::Horizontal)
+                            .constraints([
+                                Constraint::Length(width),
+                                Constraint::Min(0),
+                            ])
+                            .split(bottom)[0]
+                    },
+                    PopupPosition::Bottom => {
+                        let bottom = Layout::default()
+                            .direction(Direction::Vertical)
+                            .constraints([
+                                Constraint::Min(0),
+                                Constraint::Length(height),
+                            ])
+                            .split(max_size)[1];
+
+                        centered_rect(CenterPosition::AbsoluteInner(width, height), bottom)
+                    },
+                    PopupPosition::BottomRight => {
+                        let bottom = Layout::default()
+                            .direction(Direction::Vertical)
+                            .constraints([
+                                Constraint::Min(0),
+                                Constraint::Length(height),
+                            ])
+                            .split(max_size)[1];
+
+                        Layout::default()
+                            .direction(Direction::Horizontal)
+                            .constraints([
+                                Constraint::Min(0),
+                                Constraint::Length(width),
+                            ])
+                            .split(bottom)[1]
+                    },
+                }
             },
         }
     }
